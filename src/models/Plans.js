9cd1479089852c94a61f93ee0d6d68d5a02ca9b5
Diego Tavares <mello.gredy@gmail.com>
2022-08-16 01:08:00 -0300

    Criando model

--- src/models/Plans.js
+++ src/models/Plans.js
@@ -1,1 +1,9 @@
 // Parte Diego
+export class Plan {
+  constructor(tipo_plano, descricao, valor, duracao_plano) {
+    this.tipo_plano = tipo_plano;
+    this.descricao = descricao;
+    this.valor = valor;
+    this.duracao_plano = duracao_plano;
+  }
+}
