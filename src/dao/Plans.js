ff9dee1b2636a21a007a6e8724281a739578feca
Diego Tavares <mello.gredy@gmail.com>
2022-08-16 00:54:19 -0300

    Atualizando o DAO

--- src/dao/Plans.js
+++ src/dao/Plans.js
@@ -1,1 +1,64 @@
 // Parte Diego
+
+import { bd } from "../infra/ControlDatabase.js";
+
+const getAllPlans = () => {
+  return new Promise((resolve, reject) => {
+    bd.all("SELECT * FROM PLANS", (erro, rows) => {
+      erro ? reject(erro.message) : resolve(rows);
+    });
+  });
+};
+
+const getPlansByID = (id) => {
+  return new Promise((resolve, reject) => {
+    bd.all(`SELECT * FROM PLANS WHERE ID = ?`, id, (erro, rows) => {
+      erro ? reject(erro.message) : resolve(rows);
+    });
+  });
+};
+
+const createPlans = (listaAtributos) => {
+  return new Promise((resolve, reject) => {
+    bd.run(
+      `INSERT INTO PLANS (TIPO_PLANO, DESCRICAO, VALOR, DURACAO_PLANOS)
+          VALUES (?,?,?,?)`,
+      [...listaAtributos],
+      (erro) =>
+        erro ? reject(erro.message) : resolve("Plans created successfully!")
+    );
+  });
+};
+
+const updatePlansByID = (listaAtributosComId) => {
+  return new Promise((resolve, reject) => {
+    bd.run(
+      `UPDATE PLANS
+            SET TIPO_PLANOS = ?, DESCRICAO = ?, VALOR = ?, DURACAO_PLANOS = ?  
+              WHERE ID = ?`,
+      [...listaAtributosComId],
+      (erro) =>
+        erro
+          ? reject(erro.message)
+          : resolve("🎉🎉Plans updated successfully🎉🎉")
+    );
+  });
+};
+
+const deletePlansByID = (id) => {
+  return new Promise((resolve, reject) => {
+    bd.run(`DELETE FROM PLANS WHERE ID = ?`, id, (erro) =>
+      erro
+        ? reject(erro.message)
+        : resolve("🎉🎉Plans deleted successfully🎉🎉")
+    );
+  });
+};
+
+export {
+  getAllPlans,
+  getPlansByID,
+  createPlans,
+  updatePlansByID,
+  deletePlansByID,
+};
