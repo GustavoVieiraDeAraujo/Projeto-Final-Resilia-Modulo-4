1403ac64a2b1c4364a0307edf7595f2ab88f2258
marlonalvss <marlon14br@gmail.com>
2022-08-16 12:13:07 -0300

    :tada: init

--- src/main.js
+++ src/main.js
@@ -1,24 +1,17 @@
-// !!!!! TOMAR CUIDADO AQUI !!!!!
 
-// Importando express
 import express from "express"
 import cors from "cors";
 
-// Instanciando express
+
 const app = express()
 
-// Middleware
 app.use(express.json())
 
-// Limitando usuarios da API
+
 app.use(cors(
     {
     origin: "*"
     }
 ))
 
-//inserir codigo a partir daqui
-
-
-// Porta que API vai rodar
 app.listen(3000, () => console.log("API rodando na porta 3000"))
